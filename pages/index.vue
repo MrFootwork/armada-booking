078331738d3dc11c513466e0ccad4963810bb881
MrFootwork <pandau.ting@gmail.com>
2023-01-26 23:47:04 +0100

    built login form

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
-    <span>loading...</span>
+    <span>{{ loadingText }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
 
+const loadingText = ref('loading')
+
 onMounted(() => {
+
+  setInterval(() => {
+    if (loadingText.value.includes('...')) return loadingText.value = 'loading'
+    loadingText.value += '.'
+  }, 500)
+
   setTimeout(() => {
     navigateTo('/login')
-  }, 3000)
+  }, 4000)
 })
 </script>
 
 <style lang="scss">
 body {
   background-color: $background-color;
-}
-</style>
+  color: $font-color;
+  @import url('https://fonts.googleapis.com/css2?family=Cabin&display=swap');
+  font-family: 'Cabin', sans-serif;
 
-<style scoped lang="scss">
-div {
   display: flex;
   align-items: center;
   justify-content: center;
 
   height: 100vh;
+
+}
+</style>
+
+<style scoped lang="scss">
+div {
+  width: 5rem;
 
   span {
     color: $font-color;
--- pages/login.vue
+++ pages/login.vue
@@ -5,7 +5,8 @@
 <template>
   <div class="wrapper">
     <div class="card">
-      <span>test</span>
+      <input type="password" name="password" id="password" placeholder="Enter password here!">
+      <button>Login</button>
     </div>
   </div>
 </template>
@@ -21,8 +22,52 @@ div.wrapper {
   div.card {
     @include cardStyle;
 
-    span {
+    width: clamp(10rem, 50vw, 20rem);
+    translate: 0 -16.5vh;
+
+    input {
+      border: none;
+      padding: .5rem;
+      border-radius: $round-corner;
+      box-sizing: border-box;
+
+      width: 100%;
+
       color: $font-color;
+      text-align: center;
+
+      background-color: $card-color;
+      box-shadow:
+        inset 2px 2px 5px 1px #222,
+        inset -2px -2px 5px -3px #777;
+    }
+
+    button {
+      border: none;
+      padding: .5rem;
+      border-radius: 5px;
+      margin-top: 3rem;
+
+      width: 100%;
+      cursor: pointer;
+
+      color: $font-color;
+      text-align: center;
+
+      background-color: $card-color;
+      box-shadow:
+        2px 2px 5px 1px #222,
+        -2px -2px 5px -3px #777;
+
+      &:hover,
+      &:focus {
+        background-color: $button-color;
+      }
+
+      &:active {
+        background-color: $button-color;
+        transform: translateY(2px);
+      }
     }
   }
 }
