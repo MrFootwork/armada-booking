8ef335d265db8490f593b6aef9995341e3bddd0f
MrFootwork <pandau.ting@gmail.com>
2023-01-26 21:35:58 +0100

    setup scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ export default defineNuxtConfig({
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: '@use "@/assets/_colors.scss" as *;',
-					additionalData: '@use "@/assets/_variables.scss" as *;',
+					additionalData: '@use "@/assets/scss/_colors.scss" as *;',
+					additionalData: '@use "@/assets/scss/_variables.scss" as *;',
 				},
 			},
 		},
