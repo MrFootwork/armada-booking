64e3b868206cdf90407d9e9bf372f74a9ec7e809
MrFootwork <pandau.ting@gmail.com>
2023-04-16 23:32:41 +0200

    next: write Pinia Store for bookingDays

--- store/bookingDays.ts
+++ store/bookingDays.ts
@@ -11,5 +11,13 @@ export const useDays = defineStore('days', () => {
 		anyState.value = newState
 	}
 
+	function fetchDays() {
+		// FIXME get days and save them here
+	}
+
+	function addSlot() {
+		// FIXME add new slot to DB
+	}
+
 	return { days, anyState, setState }
 })
