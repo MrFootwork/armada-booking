6f78c7fdde81099348db7d25e44244828c1cd525
MrFootwork <pandau.ting@gmail.com>
2023-05-22 11:05:50 +0200

    implementing slot.put

--- server/api/slot.put.ts
+++ server/api/slot.put.ts
@@ -1,4 +1,8 @@
 // FIXME api endpoint for posting a single specific slot
+import { MongoClient } from 'mongodb'
+// import replaceId from '@/server/utils/mongo/replaceId'
+import { Day } from '@/model/TDay.model'
+import { ISODateString } from 'next-auth'
 
 export default defineEventHandler(async event => {
 	const queryObject = getQuery(event)
@@ -8,8 +12,8 @@ export default defineEventHandler(async event => {
 	// update document on MongoDB
 	// return updated document
 
-	// const slotInserted = await insertDay(newDate)
-	const slotInserted = 'that worked 🙌🥳🥂🥂'
+	const slotInserted = await putSlot(queryObject)
+	// const slotInserted = 'that worked 🙌🥳🥂🥂'
 
 	return {
 		api: 'slot.put',
@@ -17,3 +21,34 @@ export default defineEventHandler(async event => {
 		out: slotInserted,
 	}
 })
+
+async function putSlot(queryObject) {
+	const { mongoURI } = useRuntimeConfig()
+	const mongoClient: MongoClient = new MongoClient(mongoURI)
+
+	try {
+		await mongoClient.connect()
+		const db = mongoClient.db('bookings')
+
+		// const daysFetched = (await db
+		// 	.collection('days')
+		// 	.find({
+		// 		date: {
+		// 			$gte: new Date(query.from),
+		// 			$lte: new Date(query.to),
+		// 		},
+		// 	})
+		// 	.toArray()) as Day[]
+
+		// const daysIdTransformed = await replaceId(daysFetched)
+
+		// days holds date types
+		// return daysIdTransformed
+		return 'that worked 🙌🥳🥂🥂'
+	} catch (e) {
+		console.error('could not put on database. ', e)
+	} finally {
+		await mongoClient.close()
+		console.log('Added new slot to database.')
+	}
+}
