7f865206cea167323bd31c994e82251faee8d1ff
MrFootwork <pandau.ting@gmail.com>
2023-05-29 23:55:23 +0200

    trying out using TemplatePromise from vueuse

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { createTemplatePromise } from '@vueuse/core'
+
 import { useDaysStore } from '@/store/bookingDays'
 import { useLanguage } from '@/store/language'
 
@@ -85,6 +87,21 @@ function setDuration(inputDuration: number) {
   selectedDuration.value = inputDuration
   console.log('duration set to ', selectedDuration.value);
 }
+
+function resolveDialog(resolve, reject) {
+  setTimeout(() => {
+    resolve('foo');
+  }, 300);
+}
+
+type DialogResult = 'ok' | 'cancel'
+
+const TemplatePromise = createTemplatePromise<DialogResult, [string]>({
+  transition: {
+    name: 'fade',
+    appear: true,
+  },
+})
 
 // grid coordinates columns: wrapper slots
 const columnFirstPlayer = 2
@@ -177,8 +194,10 @@ function slotsFreeCreate() {
       const courtId = props.courtId
       const start = hour
 
+
       selectedHour.value = hour
       showDurationModal.value = true
+      const result = await TemplatePromise.start(`Hello Test`)
 
       // FIXME await for input? see link above
       const end = hour + 1
@@ -280,6 +299,34 @@ onUpdated(() => {
   <div class="wrapper schedule">
 
     <button @click="showDurationModal = true">Toggle Duration Modal</button>
+
+    <!-- <TemplatePromise v-slot="{ resolve, args, isResolving }">
+      <ScheduleModalDuration v-show="showDurationModal"
+                             :show-modal="showDurationModal"
+                             :slot-start="selectedHour"
+                             :gym-end="currentGym?.end"
+                             @toggle-modal="toggleDurationModal"
+                             @confirm-duration="setDuration" />
+
+      <div class="fixed inset-0 bg-black/10 flex items-center z-30">
+        <dialog open
+                class="border-gray/10 shadow rounded ma">
+          <div>Dialog {{ args[0] }}</div>
+          <p>Open console to see logs</p>
+          <div class="flex gap-2 justify-end">
+            <button class="w-35"
+                    @click="resolve('cancel')">
+              Cancel
+            </button>
+            <button class="w-35"
+                    :disabled="isResolving"
+                    @click="resolve(asyncFn())">
+              {{ isResolving ? 'Confirming...' : 'OK' }}
+            </button>
+          </div>
+        </dialog>
+      </div>
+    </TemplatePromise> -->
 
     <ScheduleModalDuration v-show="showDurationModal"
                            :show-modal="showDurationModal"
