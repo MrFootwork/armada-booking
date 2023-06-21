beb7426aa072cef12b08343d738a02ff972e6044
MrFootwork <pandau.ting@gmail.com>
2023-06-21 11:09:44 +0200

    🐞 found pages access by URL bug
    in prod: user enters URL, e.g. /admin and gets 500 error page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,183 +1,198 @@
 <script setup lang="ts">
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-import { useDaysStore } from '@/store/bookingDays'
-import useDate from '@/composables/date'
+	import VueDatePicker from '@vuepic/vue-datepicker'
+	import '@vuepic/vue-datepicker/dist/main.css'
+	import { useDaysStore } from '@/store/bookingDays'
+	import useDate from '@/composables/date'
 
-const fetchResult = ref()
+	// BUG🐞 in Production: can't navigate to this page
+	// => set admin access and enable admin pages
+	// => error handle this 500 error
 
-// days
-const dayStore = useDaysStore()
-const { days } = storeToRefs(dayStore)
-const { fetchDays, addSlot } = dayStore
+	const fetchResult = ref()
 
-async function getDays(from: Date) {
-  fetchResult.value = await fetchDays(from)
-}
+	// days
+	const dayStore = useDaysStore()
+	const { days } = storeToRefs(dayStore)
+	const { fetchDays, addSlot } = dayStore
 
-// FIXME delete one day or all days
-async function deleteDays(option: 'last' | 'all') {
-  const url = `/api/days?option=${option}`
-  const deleteResult = await $fetch(url, { method: 'DELETE' })
+	async function getDays(from: Date) {
+		fetchResult.value = await fetchDays(from)
+	}
 
-  fetchResult.value = deleteResult
-}
+	// FIXME delete one day or all days
+	async function deleteDays(option: 'last' | 'all') {
+		const url = `/api/days?option=${option}`
+		const deleteResult = await $fetch(url, { method: 'DELETE' })
 
-async function addDay(newDate: Date) {
-  const year = newDate.getFullYear().toString()
-  const month = newDate.getMonth().toString()
-  const day = newDate.getDate().toString()
+		fetchResult.value = deleteResult
+	}
 
-  const dateComponents = { year, month, day }
+	async function addDay(newDate: Date) {
+		const year = newDate.getFullYear().toString()
+		const month = newDate.getMonth().toString()
+		const day = newDate.getDate().toString()
 
-  const insertedDay = await $fetch(
-    `/api/days?${new URLSearchParams(dateComponents).toString()}`,
-    { method: 'POST' })
+		const dateComponents = { year, month, day }
 
-  fetchResult.value = insertedDay
-}
+		const insertedDay = await $fetch(
+			`/api/days?${new URLSearchParams(dateComponents).toString()}`,
+			{ method: 'POST' }
+		)
 
-const addSlotInStore = async (targetDate: Date) => {
-  const day = days.value.find(d => d.date.getDate() === targetDate.getDate())
-  const dayId = day!.id
-  const gymId = day!.gyms[1].id
-  const courtId = '1'
-  const start = 12
-  const end = 15
+		fetchResult.value = insertedDay
+	}
 
-  const queryObject = { dayId, gymId, courtId, start, end }
-  console.log(queryObject);
+	const addSlotInStore = async (targetDate: Date) => {
+		const day = days.value.find(d => d.date.getDate() === targetDate.getDate())
+		const dayId = day!.id
+		const gymId = day!.gyms[1].id
+		const courtId = '1'
+		const start = 12
+		const end = 15
 
-  const response = await addSlot(queryObject)
-  fetchResult.value = response
+		const queryObject = { dayId, gymId, courtId, start, end }
+		console.log(queryObject)
 
-  await fetchDays(new Date())
+		const response = await addSlot(queryObject)
+		fetchResult.value = response
 
-  console.log('updated day: ', days.value);
-}
+		await fetchDays(new Date())
 
-async function addDayWithSample(newDate: Date) {
-  const year = newDate.getFullYear().toString()
-  const month = newDate.getMonth().toString()
-  const day = newDate.getDate().toString()
-  const withSample = new Boolean(true).toString()
+		console.log('updated day: ', days.value)
+	}
 
-  const dateComponents = { year, month, day, withSample }
+	async function addDayWithSample(newDate: Date) {
+		const year = newDate.getFullYear().toString()
+		const month = newDate.getMonth().toString()
+		const day = newDate.getDate().toString()
+		const withSample = new Boolean(true).toString()
 
-  const insertedDay = await $fetch(
-    `/api/days?${new URLSearchParams(dateComponents).toString()}`,
-    { method: 'POST' })
+		const dateComponents = { year, month, day, withSample }
 
-  fetchResult.value = insertedDay
-}
+		const insertedDay = await $fetch(
+			`/api/days?${new URLSearchParams(dateComponents).toString()}`,
+			{ method: 'POST' }
+		)
 
-const today = new Date()
-const year = today.getFullYear()
-const month = today.getMonth()
-const day = today.getDate()
-const daySelected = ref(new Date(year, month, day))
+		fetchResult.value = insertedDay
+	}
 
-async function resetDays() {
-  // await $fetch('/api/days', { method: 'DELETE' })
-  const fetchedDays = await $fetch('/api/days', { method: 'POST' })
+	const today = new Date()
+	const year = today.getFullYear()
+	const month = today.getMonth()
+	const day = today.getDate()
+	const daySelected = ref(new Date(year, month, day))
 
-  fetchResult.value = fetchedDays
-}
+	async function resetDays() {
+		// await $fetch('/api/days', { method: 'DELETE' })
+		const fetchedDays = await $fetch('/api/days', { method: 'POST' })
 
+		fetchResult.value = fetchedDays
+	}
 </script>
 
 <template>
-  <div>
-    <button @click="getDays(daySelected)">days.get</button>
-    <button @click="addDay(daySelected)">days.post</button>
-    <button @click="addDayWithSample(daySelected)">days.post with sample</button>
-    <button @click="deleteDays('all')">days.delete.all</button>
-    <button @click="resetDays()">days.reset</button>
-    <button @click="addSlotInStore(daySelected)"
-            style=" background-color: orangered; 
-                    color: white">
-      slot.post
-    </button>
+	<div>
+		<button @click="getDays(daySelected)">days.get</button>
+		<button @click="addDay(daySelected)">days.post</button>
+		<button @click="addDayWithSample(daySelected)">
+			days.post with sample
+		</button>
+		<button @click="deleteDays('all')">days.delete.all</button>
+		<button @click="resetDays()">days.reset</button>
+		<button
+			@click="addSlotInStore(daySelected)"
+			style="background-color: orangered; color: white"
+		>
+			slot.post
+		</button>
 
-    <VueDatePicker v-model="daySelected"
-                   week-numbers="iso"
-                   auto-apply
-                   :clearable="false"
-                   :enable-time-picker="false"
-                   disable-month-year-select
-                   input-class-name="dp-custom-input" />
+		<VueDatePicker
+			v-model="daySelected"
+			week-numbers="iso"
+			auto-apply
+			:clearable="false"
+			:enable-time-picker="false"
+			disable-month-year-select
+			input-class-name="dp-custom-input"
+		/>
 
-    <button @click="daySelected = new Date()">Today</button>
+		<button @click="daySelected = new Date()">Today</button>
 
-    <br><span>JS Date: {{ daySelected }}</span>
-    <br><span>ISO: {{ daySelected.toISOString() }}</span>
-    <br><span>String: {{ daySelected.toString() }}</span>
-    <br><span>UTC: {{ daySelected.toUTCString() }}</span>
-    <br><span>useDate().resetTime(): {{ useDate(daySelected).resetTime() }}</span>
+		<br /><span>JS Date: {{ daySelected }}</span> <br /><span
+			>ISO: {{ daySelected.toISOString() }}</span
+		>
+		<br /><span>String: {{ daySelected.toString() }}</span> <br /><span
+			>UTC: {{ daySelected.toUTCString() }}</span
+		>
+		<br /><span
+			>useDate().resetTime(): {{ useDate(daySelected).resetTime() }}</span
+		>
 
-    <div class="days-wrapper">
-      <div class="day-item"
-           v-for="day in days">
-        {{ day.date.toLocaleString('de-DE', { dateStyle: 'long', timeStyle: 'short' }) }}
-        <!-- <br> {{ day.date }} -->
-        <br> {{ day.date.toISOString() }}
-        <br>{{ day.id }}
-        <br> type: {{ typeof day.date }}
-      </div>
-    </div>
+		<div class="days-wrapper">
+			<div class="day-item" v-for="day in days">
+				{{
+					day.date.toLocaleString('de-DE', {
+						dateStyle: 'long',
+						timeStyle: 'short',
+					})
+				}}
+				<!-- <br> {{ day.date }} -->
+				<br />
+				{{ day.date.toISOString() }} <br />{{ day.id }} <br />
+				type: {{ typeof day.date }}
+			</div>
+		</div>
 
-    <pre>{{ fetchResult }}</pre>
-
-  </div>
+		<pre>{{ fetchResult }}</pre>
+	</div>
 </template>
 
 <style scoped lang="scss">
-div.days-wrapper {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-wrap: wrap;
+	div.days-wrapper {
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		flex-wrap: wrap;
 
-  div.day-item {
-    @include buttonShadow;
-    background-color: $card-color-secondary-light;
-    border-radius: 10px;
-    padding: 1rem;
+		div.day-item {
+			@include buttonShadow;
+			background-color: $card-color-secondary-light;
+			border-radius: 10px;
+			padding: 1rem;
+		}
+	}
 
-  }
+	button {
+		border: none;
+		padding: 0.5rem;
+		margin: 0.5rem;
+		border-radius: 5px;
 
-}
+		cursor: pointer;
 
-button {
-  border: none;
-  padding: .5rem;
-  margin: .5rem;
-  border-radius: 5px;
+		color: var(--font-color);
+		text-align: center;
 
-  cursor: pointer;
+		background-color: var(--card-color-primary);
+		@include buttonShadow();
 
-  color: var(--font-color);
-  text-align: center;
+		&:hover,
+		&:focus {
+			background-color: var(--highlight-color);
+			color: var(--button-font-hover);
+		}
 
-  background-color: var(--card-color-primary);
-  @include buttonShadow();
+		&:active {
+			background-color: var(--highlight-color);
+			color: var(--button-font-hover);
+			transform: translateY(2px);
+		}
+	}
 
-  &:hover,
-  &:focus {
-    background-color: var(--highlight-color);
-    color: var(--button-font-hover);
-  }
-
-  &:active {
-    background-color: var(--highlight-color);
-    color: var(--button-font-hover);
-    transform: translateY(2px);
-  }
-}
-
-pre {
-  background-color: var(--card-color-primary);
-  border-radius: 15px;
-  padding: 1rem;
-}
+	pre {
+		background-color: var(--card-color-primary);
+		border-radius: 15px;
+		padding: 1rem;
+	}
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,150 +1,155 @@
 <script setup lang="ts">
-// page behavior 
-definePageMeta({
-  auth: {
-    // guests can access this page
-    unauthenticatedOnly: true,
-    // logged in users are redirected to
-    navigateAuthenticatedTo: '/days',
-  }
-})
+	// page behavior
+	definePageMeta({
+		auth: {
+			// guests can access this page
+			unauthenticatedOnly: true,
+			// logged in users are redirected to
+			navigateAuthenticatedTo: '/days',
+		},
+	})
 
-const { signIn } = useAuth()
+	const { signIn } = useAuth()
 
-// input data
-const username = ref('')
-const password = ref('')
-// error message is set in case of any failures during sign-in
-// list of possible errors
-// https://github.com/nextauthjs/next-auth/blob/aad0b8db0e8a163b3c3ae7dec3e9158e20d368f4/packages/next-auth/src/core/pages/signin.tsx#L4-L19
-const signInErrorMessage = ref('')
+	// input data
+	const username = ref('')
+	const password = ref('')
+	// error message is set in case of any failures during sign-in
+	// list of possible errors
+	// https://github.com/nextauthjs/next-auth/blob/aad0b8db0e8a163b3c3ae7dec3e9158e20d368f4/packages/next-auth/src/core/pages/signin.tsx#L4-L19
+	const signInErrorMessage = ref('')
 
-const signInHandler = async ({ username, password }: { username: string, password: string }) => {
+	const signInHandler = async ({
+		username,
+		password,
+	}: {
+		username: string
+		password: string
+	}) => {
+		const { error, url } = await signIn('credentials', {
+			username,
+			password,
+			redirect: false,
+		})
 
-  const { error, url } = await signIn('credentials', { username, password, redirect: false })
-
-  if (error) {
-    signInErrorMessage.value = error
-  } else {
-    // No error, continue with the sign in, e.g., by following the returned redirect:
-    return navigateTo(url, { external: true })
-  }
-}
-
+		if (error) {
+			signInErrorMessage.value = error
+		} else {
+			// No error, continue with the sign in, e.g., by following the returned redirect:
+			return navigateTo(url, { external: true })
+		}
+	}
 </script>
 
 <template>
-  <div class="wrapper">
+	<div class="wrapper">
+		<img class="logo" src="/logo.png" alt="logo" />
 
-    <img class="logo"
-         src="/logo.png"
-         alt="logo">
+		<form class="login" @submit.prevent>
+			<div v-show="signInErrorMessage">
+				<p
+					v-if="signInErrorMessage === 'CredentialsSignin'"
+					class="error-credentials-signin"
+				>
+					Please check your credentials! They seem to be wrong.
+					<br /><br />Error Code: {{ signInErrorMessage }}
+				</p>
+				<p v-else class="error-credentials-signin">
+					Something unexpected happened. Please advise an administrator!
+					<br /><br />Error Code: <br />{{ signInErrorMessage }}
+				</p>
+			</div>
 
-    <form class="login"
-          @submit.prevent>
+			<input
+				type="text"
+				name="username"
+				id="username"
+				placeholder="hint: jsmith"
+				v-model="username"
+			/>
 
-      <div v-show="signInErrorMessage">
-        <p v-if="signInErrorMessage === 'CredentialsSignin'"
-           class="error-credentials-signin">
-          Please check your credentials! They seem to be wrong.
-          <br><br>Error Code: {{ signInErrorMessage }}
-        </p>
-        <p v-else
-           class="error-credentials-signin">
-          Something unexpected happened. Please advise an administrator!
-          <br><br>Error Code:
-          <br>{{ signInErrorMessage }}
-        </p>
-      </div>
+			<input
+				type="password"
+				name="password"
+				id="password"
+				placeholder="hint: hunter2"
+				v-model="password"
+			/>
 
-      <input type="text"
-             name="username"
-             id="username"
-             placeholder="hint: jsmith"
-             v-model="username">
-
-      <input type="password"
-             name="password"
-             id="password"
-             placeholder="hint: hunter2"
-             v-model="password">
-
-      <button @click="signInHandler({ username, password })">Login</button>
-
-    </form>
-
-  </div>
+			<button @click="signInHandler({ username, password })">Login</button>
+		</form>
+	</div>
 </template>
 
 <style scoped lang="scss">
-div.wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-flow: column;
+	div.wrapper {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		flex-flow: column;
 
-  height: calc(100vh - 6rem);
+		height: calc(100vh - 6rem);
 
-  img.logo {
-    width: 12rem;
-    margin: 3rem;
-  }
+		img.logo {
+			width: 12rem;
+			margin: 3rem;
+		}
 
-  form.login {
-    @include cardStyle;
-    outline: none;
+		form.login {
+			@include cardStyle;
+			outline: none;
 
-    width: clamp(360px, 50vw, 400px);
+			width: clamp(360px, 50vw, 400px);
 
-    .error-credentials-signin {
-      padding: 1rem;
-      margin: 0;
-      border-radius: .5rem;
-      background-color: var(--highlight-color);
-      color: $font-color-dark;
-    }
+			.error-credentials-signin {
+				padding: 1rem;
+				margin: 0;
+				border-radius: 0.5rem;
+				background-color: var(--highlight-color);
+				color: $font-color-dark;
+			}
 
-    input {
-      border: none;
-      padding: .5rem;
-      border-radius: $round-corner;
-      box-sizing: border-box;
+			input {
+				border: none;
+				padding: 0.5rem;
+				border-radius: $round-corner;
+				box-sizing: border-box;
 
-      width: 100%;
+				width: 100%;
 
-      color: var(--font-color);
-      text-align: center;
+				color: var(--font-color);
+				text-align: center;
 
-      background-color: var(--card-color-primary);
-      @include inputShadow();
-    }
+				background-color: var(--card-color-primary);
+				@include inputShadow();
+			}
 
-    button {
-      border: none;
-      padding: .5rem;
-      border-radius: 5px;
+			button {
+				border: none;
+				padding: 0.5rem;
+				border-radius: 5px;
 
-      width: 100%;
-      cursor: pointer;
+				width: 100%;
+				cursor: pointer;
 
-      color: var(--font-color);
-      text-align: center;
+				color: var(--font-color);
+				text-align: center;
 
-      background-color: var(--card-color-primary);
-      @include buttonShadow();
+				background-color: var(--card-color-primary);
+				@include buttonShadow();
 
-      &:hover,
-      &:focus {
-        background-color: var(--highlight-color);
-        color: var(--button-font-hover);
-      }
+				&:hover,
+				&:focus {
+					background-color: var(--highlight-color);
+					color: var(--button-font-hover);
+				}
 
-      &:active {
-        background-color: var(--highlight-color);
-        color: var(--button-font-hover);
-        transform: translateY(2px);
-      }
-    }
-  }
-}
+				&:active {
+					background-color: var(--highlight-color);
+					color: var(--button-font-hover);
+					transform: translateY(2px);
+				}
+			}
+		}
+	}
 </style>
