2e3cee66665b5a8484a86faa1335b320ff1c04f7
MrFootwork <pandau.ting@gmail.com>
2023-06-22 23:47:16 +0200

    ✅ testing scheduler

--- server/plugins/schedulerNewDay.ts
+++ server/plugins/schedulerNewDay.ts
@@ -12,43 +12,44 @@ function fillWeekWithDays() {
 	const scheduler = useScheduler()
 
 	// FIXME implement this scheduler
-	scheduler.run(async () => {
-		const { today, lastDay } = currentWeekInRomania()
+	scheduler
+		.run(async () => {
+			const { today, lastDay } = currentWeekInRomania()
 
-		// get days on DB
-		const { out: daysOnDB } = await $fetch('/api/days', {
-			method: 'GET',
-			params: { from: today.toISOString(), to: lastDay.toISOString() },
+			// get days on DB
+			const { out: daysOnDB } = await $fetch('/api/days', {
+				method: 'GET',
+				params: { from: today.toISOString(), to: lastDay.toISOString() },
+			})
+
+			// determine missing days of the week
+			const missingDays = getMissingDays(daysOnDB, today, lastDay)
+
+			if (!missingDays) return
+
+			// loop through missingDays and generate new day
+			missingDays.forEach(async missingDay => {
+				console.log(
+					'🚀 ~ file: schedulerNewDay.ts:30 ~ missingDays.forEach ~ missingDay.date:',
+					missingDay
+				)
+				const year = missingDay.getFullYear().toString()
+				const month = missingDay.getMonth().toString()
+				const day = missingDay.getDate().toString()
+				// const year = new Date(missingDay).getFullYear().toString()
+				// const month = new Date(missingDay).getMonth().toString()
+				// const day = new Date(missingDay).getDate().toString()
+
+				const dateComponents = { year, month, day }
+
+				const insertedDay = await $fetch(
+					`/api/days?${new URLSearchParams(dateComponents).toString()}`,
+					{ method: 'POST' }
+				)
+			})
 		})
-
-		// determine missing days of the week
-		const missingDays = getMissingDays(daysOnDB, today, lastDay)
-
-		if (!missingDays) return
-
-		// loop through missingDays and generate new day
-		missingDays.forEach(async missingDay => {
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:30 ~ missingDays.forEach ~ missingDay.date:',
-				missingDay
-			)
-			const year = missingDay.getFullYear().toString()
-			const month = missingDay.getMonth().toString()
-			const day = missingDay.getDate().toString()
-			// const year = new Date(missingDay).getFullYear().toString()
-			// const month = new Date(missingDay).getMonth().toString()
-			// const day = new Date(missingDay).getDate().toString()
-
-			const dateComponents = { year, month, day }
-
-			const insertedDay = await $fetch(
-				`/api/days?${new URLSearchParams(dateComponents).toString()}`,
-				{ method: 'POST' }
-			)
-		})
-	})
-	// .everySeconds(15)
-	// .cron('1 0 * * *', 'Europe/Bucharest')
+		// .everySeconds(15)
+		.cron('1 0 * * *', 'Europe/Bucharest')
 
 	// create as many tasks as you want here
 }
