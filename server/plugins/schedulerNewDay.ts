9b5224fff6ef2ff8b0bff3575264f849c08e98b5
MrFootwork <pandau.ting@gmail.com>
2023-06-26 22:44:02 +0200

    📦 installed luxon (successor of moment.js)

--- server/plugins/schedulerNewDay.ts
+++ server/plugins/schedulerNewDay.ts
@@ -5,11 +5,11 @@ import currentWeekInRomania from '../app/services/todayDateInRomania.ts'
 import getMissingDays from '../app/services/getMissingDays.ts'
 
 const inDevelopment = useRuntimeConfig().nodeEnvironment === 'development'
-const timeZone = 'Europe/Bucharest'
+const TIME_ZONE = 'Europe/Bucharest'
 
 export default defineNitroPlugin(() => {
-	// if (!inDevelopment) fillWeekWithDays()
-	fillWeekWithDays()
+	if (!inDevelopment) fillWeekWithDays()
+	// fillWeekWithDays()
 	console.log('scheduler is activeted ⏱')
 })
 
@@ -21,36 +21,15 @@ function fillWeekWithDays() {
 		.run(async () => {
 			console.log('Time Test')
 			const now = new Date()
-			console.log('🚀 ~ file: schedulerNewDay.ts:22 ~ .run ~ now:', now)
-
-			const offset = getOffset(timeZone)
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:26 ~ scheduler.run ~ offset:',
-				offset
-			)
+			const offset = getOffset(TIME_ZONE)
 			const isoOffset = `+${(-offset / 60).toString().padStart(2, '0')}:00`
 			// build UTC conform target date times
 
 			// BUG🐞
 
 			const { today, lastDay } = currentWeekInRomania()
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:38 ~ .run ~ today, lastDay:',
-				today,
-				lastDay
-			)
-
 			const todayCorrect = new Date(isoDateFrom(today, 0, isoOffset))
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:40 ~ .run ~ todayCorrect:',
-				todayCorrect
-			)
 			const lastDayCorrect = new Date(isoDateFrom(lastDay, 0, isoOffset))
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:42 ~ .run ~ lastDayCorrect:',
-				lastDayCorrect
-			)
-
 			// get days on DB
 			// const { out: daysOnDB } = await $fetch('/api/days', {
 			// 	method: 'GET',
@@ -66,20 +45,10 @@ function fillWeekWithDays() {
 
 			// determine missing days of the week
 			const missingDays = getMissingDays(daysOnDB, today, lastDay)
-			console.log(
-				'🚀 ~ file: schedulerNewDay.ts:40 ~ .run ~ missingDays:',
-				missingDays
-			)
-
 			if (!missingDays) return
 
 			// loop through missingDays and generate new day
 			missingDays.forEach(async missingDay => {
-				console.log(
-					'🚀 ~ file: schedulerNewDay.ts:49 ~ missingDays.forEach ~ missingDay:',
-					missingDay
-				)
-
 				const year = missingDay.getFullYear().toString()
 				const month = missingDay.getMonth().toString()
 				const day = missingDay.getDate().toString()
@@ -89,10 +58,6 @@ function fillWeekWithDays() {
 				const insertedDay = await $fetch(
 					`/api/days?${new URLSearchParams(dateComponents).toString()}`,
 					{ method: 'POST' }
-				)
-				console.log(
-					'🚀 ~ file: schedulerNewDay.ts:66 ~ .run ~ insertedDay:',
-					insertedDay
 				)
 			})
 		})
